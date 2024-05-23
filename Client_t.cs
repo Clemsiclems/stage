578bb183891022c8243c195a36b6bcc07c98bb7b
Clément <127097822+Clemsiclems@users.noreply.github.com>
2024-05-23 19:29:16 +0200

    Add files via upload

---
+++ Client_t.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Caisse.Models
+{
+    public class Clients_t
+    {
+        public int IDClient { get; set; }
+        public string cliNumGRC { get; set; }
+        public string cliNom { get; set; }
+        public string cliPrenom { get; set; }
+        public string cliCP { get; set; }
+        public string cliCommune { get; set; }
+        public string cliPays { get; set; }
+        public string cliIDAgence { get; set; }
+        public bool cliPart { get; set; }
+        public string cliTotContratIARD { get; set; }
+        public string cliManuel { get; set; }
+        public List<string> Sigma { get; set; }
+
+        public List<SelectListItem> ListeCommunes { get; set; }
+        public string SelectedCommune { get; set; }
+    }
+
+}
